989e3378be76d70ebaa4aa62c7a3de952bdf5294
Geoff Vincent <geoffkvincent@gmail.com>
2018-08-22 09:04:42 -0600

    added jquery effects

---
+++ main.js
@@ -0,0 +1,32 @@
+
+
+$(document).ready( function() {
+
+
+  var $rock = $('#rock')
+  var $paper = $('#paper')
+  var $scissor = $('#scissor')
+  var $win = $('#win')
+  var $lose = $('#lose')
+  var $tie = $('#tie')
+  var $title = $("#title")
+  var $choice = $(".choice")
+  var $options = [$rock, $paper, $scissor]
+
+  $title.fadeIn(8000)
+  $choice.fadeIn(8000)
+  $win.fadeIn(11000)
+  $lose.fadeIn(12000)
+  $tie.fadeIn(13000)
+
+  function userSelection(e) {
+
+  }
+
+  
+
+
+
+
+})
+
